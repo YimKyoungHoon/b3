dea492c724fae1f77a42f8456a0edc97143e37f8
YimKyoungHoon <chipfamily0124@gmail.com>
2023-10-26 16:32:14 +0900

    웹디자인기능사 예제(역사박물관)

---
+++ script/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    //gnb
+    $("#gnb > li").on("mouseenter", function () {
+        $(this).find("ul.sub").stop().slideDown();
+    })
+    $("#gnb >li").on("mouseleave", function () {
+        $(this).find("ul.sub").stop().slideUp();
+    })
+    //popup
+    $("#notice > ul > li").on("click", function () {
+        $("#popupBg").css({ "display": "block" });
+    })
+    $("#close").on("click", function () {
+        $("#popupBg").css({ "display": "none" })
+    })
+    //slide
+    setInterval(function () {
+        $('.frame').animate({ "top": "-100%" }, 1000, function () {
+            $('.frame > li').eq(0).appendTo('.frame');
+            $('.frame').css({ "top": "0px" })
+        })
+    }, 3000);
+})
